4293dfff1d71e4ff95e792009b40baf6b8a90d7d
Sagar <sagarsi0202@gmail.com>
2021-06-19 17:22:23 +0530

    Initial commit

---
+++ js/index.js
@@ -0,0 +1,23 @@
+// <---------------------Age Calculator--------------------->
+
+function change(){
+    var age = document.getElementById('age').value;
+    document.getElementById('month').value = (age * 12) + " Months";
+    document.getElementById('day').value = (age * 12 * 30) + " Days";
+    document.getElementById('hour').value = (age * 12 * 30 * 24) + " Hours";
+    document.getElementById('minute').value = (age * 12 * 30 * 24 * 60) + " Minutes";
+    document.getElementById('second').value = (age * 12 * 30 * 24 * 60 * 60) + " Seconds";
+}
+
+// <-----------x----------Age Calculator---------x------------>
+
+
+
+
+// <---------------------Calculator--------------------->
+
+
+
+
+// <-----------x----------Calculator---------x------------>
+
